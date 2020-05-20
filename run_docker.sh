8a2f24a7aba455dca3910497114b484a86ca1826
Vid Bregar <vid.bregar@gmail.com>
2020-05-20 07:05:28 +0200

    Create publish image script

--- run_docker.sh
+++ run_docker.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+# Builds and starts a container
 
-docker build -t ml-microservice .
+docker build -t vidbregar/ml-microservice .
 
 docker image ls
 
-docker run -p 8000:80 -d ml-microservice
+docker run -p 8000:80 -d vidbregar/ml-microservice
--- upload_docker.sh
+++ upload_docker.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
-# This file tags and uploads an image to Docker Hub
-
+# Tags and uploads an image to Docker Hub
 # Assumes that an image is built via `run_docker.sh`
 
-# Step 1:
-# Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="vidbregar/ml-microservice:latest"
 
-# Step 2:  
-# Authenticate & tag
+docker login --username=vidbregar
+docker tag ml-microservice - $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
-# Step 3:
-# Push image to a docker repository
+docker push $dockerpath
