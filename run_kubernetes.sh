a6b07c30a5d8a7780fa6cf46e775d3d0962a4466
Vid Bregar <vid.bregar@gmail.com>
2020-05-21 12:15:25 +0200

    Create kuberentes script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 
-# This tags and uploads an image to Docker Hub
+# Starts Kubernetes
 
-# Step 1:
-# This is your Docker ID/path
-# dockerpath=<>
+dockerpath="vidbregar/ml-microservice:latest"
 
-# Step 2
-# Run the Docker Hub container with kubernetes
+kubectl run ml-microservice\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=ml-microservice
 
+kubectl get pods
 
-# Step 3:
-# List kubernetes pods
-
-# Step 4:
-# Forward the container port to a host
-
+kubectl port-forward ml-microservice 8000:80
